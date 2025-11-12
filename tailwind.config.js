cc1922869ceeb65d8463710d70714e065d0fce3f
TRISHIT BODKHE <trishitbodkhe726@gmail.com>
2025-11-12 17:07:45 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#4f46e5'
+        }
+      }
+    }
+  },
+  plugins: []
+}
